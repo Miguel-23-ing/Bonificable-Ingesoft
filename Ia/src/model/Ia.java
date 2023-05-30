599dab386ae46b99fedef9ec6c85b01937ecf864
NaySp <nsp1324@hotmail.com>
2023-05-30 14:50:07 -0500

    cambio a lista

--- Ia/src/model/Ia.java
+++ Ia/src/model/Ia.java
@@ -1,23 +1,37 @@
 package model;
 
-import java.util.HashMap;
-import java.util.Map;
 
 public class Ia {
-    private Map<String, String> planetData;
+    private String[] planetNames;
+    private String[] planetData;
 
     public Ia() {
+        // Inicializa los nombres de los planetas
+        planetNames = new String[] {
+            "Mercurio",
+            "Venus",
+            "Tierra",
+            "Marte"
+            // Agrega más nombres de planetas según sea necesario
+        };
+
         // Inicializa los datos de los planetas
-        planetData = new HashMap<>();
-        planetData.put("Mercurio", "Mercurio es el planeta más cercano al Sol.");
-        planetData.put("Venus", "Venus es el segundo planeta del sistema solar y es conocido como el planeta hermano de la Tierra.");
-        planetData.put("Tierra", "La Tierra es nuestro planeta, el tercero del sistema solar y el único conocido que alberga vida.");
-        planetData.put("Marte", "Marte es el cuarto planeta del sistema solar y es conocido como el planeta rojo debido a su color característico.");
-        // Agrega más datos de los planetas según sea necesario
+        planetData = new String[] {
+            "Mercurio es el planeta más cercano al Sol.",
+            "Venus es el segundo planeta del sistema solar y es conocido como el planeta hermano de la Tierra.",
+            "La Tierra es nuestro planeta, el tercero del sistema solar y el único conocido que alberga vida.",
+            "Marte es el cuarto planeta del sistema solar y es conocido como el planeta rojo debido a su color característico."
+            // Agrega más datos de los planetas según sea necesario
+        };
     }
 
     public String getPlanetInfo(String planet) {
-        return planetData.getOrDefault(planet, "Lo siento, no tengo información sobre ese planeta.");
+        for (int i = 0; i < planetNames.length; i++) {
+            if (planetNames[i].equalsIgnoreCase(planet)) {
+                return planetData[i];
+            }
+        }
+        return "Lo siento, no tengo información sobre ese planeta.";
     }
 
 }
