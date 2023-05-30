97bea0d8e0f1046c7c37599e9b07b95b80c600ce
NaySp <nsp1324@hotmail.com>
2023-05-30 14:38:57 -0500

    bases

--- Ia/src/model/Ia.java
+++ Ia/src/model/Ia.java
@@ -1,4 +1,23 @@
 package model;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Ia {
+    private Map<String, String> planetData;
+
+    public Ia() {
+        // Inicializa los datos de los planetas
+        planetData = new HashMap<>();
+        planetData.put("Mercurio", "Mercurio es el planeta más cercano al Sol.");
+        planetData.put("Venus", "Venus es el segundo planeta del sistema solar y es conocido como el planeta hermano de la Tierra.");
+        planetData.put("Tierra", "La Tierra es nuestro planeta, el tercero del sistema solar y el único conocido que alberga vida.");
+        planetData.put("Marte", "Marte es el cuarto planeta del sistema solar y es conocido como el planeta rojo debido a su color característico.");
+        // Agrega más datos de los planetas según sea necesario
+    }
+
+    public String getPlanetInfo(String planet) {
+        return planetData.getOrDefault(planet, "Lo siento, no tengo información sobre ese planeta.");
+    }
+
 }
--- Ia/src/ui/Main.java
+++ Ia/src/ui/Main.java
@@ -1,7 +1,33 @@
 package ui;
+import model.*;
+import java.util.Scanner;
+
 
 public class Main {
+
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Ia prototype = new Ia();
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("¡Bienvenido al prototipo de preguntas y respuestas sobre planetas!");
+        System.out.println("Puedes hacer preguntas sobre los planetas del sistema solar. Escribe 'salir' para terminar.");
+
+        String input;
+        while (true) {
+            System.out.print("\nIngresa tu pregunta: ");
+            input = scanner.nextLine();
+
+            if (input.equalsIgnoreCase("salir")) {
+                System.out.println("¡Hasta luego!");
+                break;
+            }
+
+            String planetInfo = prototype.getPlanetInfo(input);
+            System.out.println("Respuesta: " + planetInfo);
+        }
+
+        scanner.close();
     }
+
 }
